9c2a05e8e045a8a109e97951bafb408a61cc6c82
Sam <romeosamzy@gmail.com>
2022-06-09 14:22:24 +0800

    Finished 15-Cocktails

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis hic
+        nulla fugiat fugit voluptatibus natus, aliquam aspernatur ea, veniam
+        earum aut? Fugiat optio, impedit animi soluta consequatur culpa
+        provident eveniet ea, laudantium rerum architecto inventore eligendi
+        quidem iste quibusdam. Inventore?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
